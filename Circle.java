b8568d30508e523acb0c3a9a25224bb360c1a853
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-06-11 18:40:27 -0300

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Circle { 
+public static void main (String[] args){
+	Locale.setDefault(Locale.US);
+
+	Scanner sc = new Scanner (System.in);
+	double radius, radius2, area;
+	//fazer a area de um circulo ao receber o raio;
+System.out.println("Digite o raio do circulo");
+System.out.println("Input the radius of the circle");
+radius = sc.nextDouble();
+
+
+radius2 = radius * radius;
+area = 3.14 * radius2;
+
+System.out.printf("The area of the circle is %1.2f", area);
+
+			sc.close();											
+}
+}
